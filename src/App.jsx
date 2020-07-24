0132e28826a1656a9dac7712606b49f24f3e8d36
Ricardo Olarte <raop155@hotmail.com>
2020-07-24 15:09:28 -0500

    Budget app

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './styles/components/App.scss';
+
+import Header from './components/Header';
+import Dashboard from './views/Dashboard';
+import AddExpense from './views/AddExpense';
+import EditExpense from './views/EditExpense';
+import Help from './views/Help';
+import NotFound from './views/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path='/' component={Dashboard} exact />
+        <Route path='/create' component={AddExpense} />
+        <Route path='/edit/:id' component={EditExpense} />
+        <Route path='/help' component={Help} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
