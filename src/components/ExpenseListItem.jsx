f1c5d053a1b4f85c2b3c21dc57014805d8dc11e3
Ricardo Olarte <raop155@hotmail.com>
2020-08-12 17:01:06 -0500

    *feat* ExpenseListItem format for amount and creatAt date

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({ id, description, amount, createAt }) => {
   return (
@@ -8,7 +10,8 @@ export const ExpenseListItem = ({ id, description, amount, createAt }) => {
         <h3>{description}</h3>
       </Link>
       <p>
-        amount: {amount} - createAt: {createAt}
+        amount: {numeral(amount / 100).format('$0,0.00')} - createAt:{' '}
+        {moment(createAt).format('MMMM Do, YYYY')}
       </p>
     </div>
   );
